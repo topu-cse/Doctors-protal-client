90e7c2b7b5356d97b51b32454c4a18389f7fe658
topu12345 <topu15-3604@diu.edu.bd>
2023-02-05 22:15:48 +0600

    dashboard

---
+++ src/Pages/Dashboard/Dashboard/MyAppointment/MyAppointment.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+export class MyAppointment extends Component {
+    render() {
+        return (
+            <div>
+                <h3 className="tsxt-3xl mb-5"> My Appointment</h3>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+                            
+                            <tr className="hover">
+                                <th>2</th>
+                                <td>Hart Hagerty</td>
+                                <td>Desktop Support Technician</td>
+                                <td>Purple</td>
+                            </tr>
+                           
+                            <tr>
+                                <th>3</th>
+                                <td>Brice Swyre</td>
+                                <td>Tax Accountant</td>
+                                <td>Red</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MyAppointment
