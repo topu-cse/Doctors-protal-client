d929e728cb48c2805da2a1956462bb1f327d3142
topu12345 <topu15-3604@diu.edu.bd>
2023-02-22 00:53:09 +0600

    add Doctors

--- src/Pages/Dashboard/Adddoctors/Adddoctors.js
+++ src/Pages/Dashboard/Adddoctors/Adddoctors.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Adddoctors = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleAddDoctor = data => {
+        console.log(data)
+    }
+
     return (
-        <div>
-            
+        <div className='w-96 p-7'>
+            <h2 className='text-4xl'>Add Doctors</h2>
+            <form onSubmit={handleSubmit(handleAddDoctor)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Name</span></label>
+                    <input type="text" {...register("name", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Email</span></label>
+                    <input type="email" {...register("email", {
+                        required: true
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text"> Speacialty</span></label>
+                    <select className="select select-bordered w-full max-w-xs">
+                        <option disabled selected>Pick a Speacialty</option>
+                        <option>Han Solo</option>
+                        <option>Greedo</option>
+                    </select>
+                </div>
+                <input className='btn btn-accent w-full mt-4' value="Add Doctor" type="submit" />
+                {/* {signUpError && <p className='text-red-600'>{signUpError}</p>} */}
+            </form>
         </div>
     );
 };
