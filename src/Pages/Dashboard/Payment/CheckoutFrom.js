8ad2a1eb044f0004a037e6d1056b04d704b3b006
topu12345 <topu15-3604@diu.edu.bd>
2023-02-28 00:30:55 +0600

    payment

--- src/Pages/Dashboard/Payment/CheckoutFrom.js
+++ src/Pages/Dashboard/Payment/CheckoutFrom.js
@@ -1,7 +1,8 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const CheckoutFrom = () => {
+    const [cardError,setCardError]=useState('')
     const stripe = useStripe();
     const elements = useElements();
 
@@ -10,6 +11,23 @@ const CheckoutFrom = () => {
         if (!stripe || elements) {
             return
         }
+        const card = elements.getElement(CardElement);
+        if (card == null) {
+            return;
+          }
+          // Use your card Element with other Stripe.js APIs
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+        type: 'card',
+        card,
+      });
+   if(error){
+    console.log(error);
+    setCardError(error.message)
+   }
+   else{
+    setCardError('')
+   }
+
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -29,7 +47,7 @@ const CheckoutFrom = () => {
                     },
                 }}
             />
-            <button type="submit" disabled={!stripe}>
+            <button className='btn btn-sm mt-4' type="submit" disabled={!stripe}>
                 Pay
             </button>
         </form>
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,6 +1,8 @@
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutFrom from './CheckoutFrom';
 
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -13,6 +15,11 @@ const Payment = () => {
         <div>
             <h2 className='text-3xl'>Payment for {treatment} </h2>
             <p className='text-xl'>pleace pay ${ price} for your appointment on {appointmentDate} at {slot}</p>
+            <div className='w-96 my-12'>
+            <Elements stripe={stripePromise}>
+      <CheckoutFrom />
+    </Elements>
+            </div>
         </div>
     );
 };
