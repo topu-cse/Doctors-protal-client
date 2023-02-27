77fa66ea9ba1c538c378485a98c7a7293807bc42
topu12345 <topu15-3604@diu.edu.bd>
2023-02-27 22:23:17 +0600

    payment

--- src/Pages/Dashboard/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/Dashboard/MyAppointment/MyAppointment.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../../contexts/AuthProvider';
 
 const MyAppointment = () => {
@@ -35,6 +36,7 @@ const MyAppointment = () => {
                             <th>Treatment</th>
                             <th>Date</th>
                             <th>Time</th>
+                            <th>Payment</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -46,6 +48,19 @@ const MyAppointment = () => {
                                 <td>{booking.treatment}</td>
                                 <td>{booking.appointmentDate}</td>
                                 <td>{booking.slot}</td>
+                                <td>
+                                    {
+                                        booking.price && !booking.paid && 
+                                       <Link to={`/dashboard/payment/${booking._id}`}>
+                                       
+                                       <button className='btn btn-primary btn-sm'>pay</button>
+                                       </Link>
+                                    }
+                                    {
+                                        booking.price && booking.paid && <span className='primary'>paid</span>
+                                    }
+                                </td>
+
                             </tr>)
                         }
                     </tbody>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -6,6 +6,7 @@ import Adddoctors from "../../Pages/Dashboard/Adddoctors/Adddoctors";
 import Alusers from "../../Pages/Dashboard/AllUsers/Alusers";
 import MyAppointment from "../../Pages/Dashboard/Dashboard/MyAppointment/MyAppointment";
 import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
+import Payment from "../../Pages/Dashboard/Payment/Payment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -54,6 +55,11 @@ const router = createBrowserRouter([
             {
                 path:'/dashboard/managedoctors',
                 element:<AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
+            },
+            {
+                path:'/dashboard/payment/:id',
+                element:<AdminRoute><Payment></Payment></AdminRoute>,
+                loader:({params})=>fetch(`http://localhost:5000/booking/${params.id}`)
             },
         ]
     }
